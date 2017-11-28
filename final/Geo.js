5c02ab0b1c0816aa8103f47e34f39236f6676a78
Zachary Jones <zjones4@uic.edu>
2017-11-28 21:13:14 +0000

    Fixed Graph/Fixed Layout/Aesthetics

---
+++ final/Geo.js
@@ -0,0 +1,25 @@
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    
+    return  latlon= { lat : latitude, long : longitude
+  };
+}
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  
+}
